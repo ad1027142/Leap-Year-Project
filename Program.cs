311aff4c6ed507f7946134443ef581f2c8fdc501
ad1027142 <ad1027142@otc.edu>
2025-09-22 09:58:35 -0500

    First iteration
    The first iteration of the project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Program that detects if a year the user selects is a leap year
+// Greet the user and ask for a name
+Console.WriteLine("Hello");
+Console.WriteLine("Pick a year");
+Console.WriteLine();
+Console.Write(">> ");
+// Input the name and make it a number
+int year = Convert.ToInt32(Console.ReadLine());
+// Checks if the year is a leap year
+if ((year % 4 == 0 && !(year % 100 == 0)) || (year % 4 == 0 && year % 100 == 0 && year % 400 == 0))
+{
+    Console.WriteLine($"{year} is a leap year!");
+}
+else
+{
+    Console.WriteLine($"{year} is not leap year!");
+}
