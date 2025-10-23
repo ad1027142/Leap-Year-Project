05f102a484a1ec44e6b5f8c9f50ec9a782855070
ad1027142 <ad1027142@otc.edu>
2025-10-23 09:32:07 -0500

    Update Program.cs
    Added failsafe if they input a word

--- Program.cs
+++ Program.cs
@@ -1,17 +1,32 @@
-﻿// Program that detects if a year the user selects is a leap year
+// Program that detects if a year the user selects is a leap year
 // Greet the user and ask for a name
-Console.WriteLine("Hello");
-Console.WriteLine("Pick a year");
-Console.WriteLine();
-Console.Write(">> ");
-// Input the name and make it a number
-int year = Convert.ToInt32(Console.ReadLine());
-// Checks if the year is a leap year
-if ((year % 4 == 0 && !(year % 100 == 0)) || (year % 4 == 0 && year % 100 == 0 && year % 400 == 0))
+using System.Diagnostics;
+int years = 0;
+bool tried = false;
+while (tried == false)
 {
-    Console.WriteLine($"{year} is a leap year!");
+    Console.WriteLine("Hello");
+    Console.WriteLine("Pick a year");
+    Console.WriteLine();
+    Console.Write(">> ");
+    // Input the name and make it a number
+    string year = Console.ReadLine() + "";
+    if (int.TryParse(year, out int result))
+        {
+            years = Convert.ToInt32(year);
+            tried = true;
+        }
+    else
+        {
+            Console.WriteLine("Invalid input.");
+        }
+}
+// Checks if the year is a leap year
+if ((years % 4 == 0 && !(years % 100 == 0)) || (years % 4 == 0 && years % 100 == 0 && years % 400 == 0))
+{
+    Console.WriteLine($"{years} is a leap year!");
 }
 else
 {
-    Console.WriteLine($"{year} is not leap year!");
+    Console.WriteLine($"{years} is not leap year!");
 }
